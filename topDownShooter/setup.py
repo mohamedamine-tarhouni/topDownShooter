2b1f37ea2bc31217e494592fae45deadf00459fc
MTARHOUNI <mohamedamine.tarhouni@ynov.com>
2022-06-14 23:00:27 +0200

    keyboard+controller

--- topDownShooter/setup.py
+++ topDownShooter/setup.py
@@ -4,6 +4,6 @@ executables= [cx_Freeze.Executable("main.py")]
 cx_Freeze.setup(
     name="topDownShooter",
     options={"build_exe":{"packages":["pygame","sqlite3","sys","settings","model.player","model.bullet","model.menu"],
-    "include_files":["img/Man 1.png"]}},
+    "include_files":["img/Man 1.png","ps4_keys.json"]}},
     executables=executables
 )
